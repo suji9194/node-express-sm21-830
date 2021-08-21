91a280a8d84279c5b55837f9d49459bd33113482
“Sujitha <srajan@sea.yearup.org>
2021-08-21 21:52:14 +0000

    Add EJS test

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var app = express();
+const PORT = 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.set('view engine','ejs');//sent views to EJS
+
+//this is how a route is built in Express
+app.get('/', (req,res)=>{
+  var title = "Our Home Page";
+  res.render('index',{title:title});
+});
+
+app.listen(PORT,()=>{
+  console.log(`App is running on port ${PORT}`);
+});
