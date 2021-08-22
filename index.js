738d0140afe645cec37ac18669793d41e6d0ebbc
suji9194 <82463876+suji9194@users.noreply.github.com>
2021-08-22 04:34:37 -0700

    Adds user files

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-
 var express = require('express');
+var data = require('./data/test.json');
 
 var app = express();
 const PORT = 3000;
 
 app.set('view engine','ejs');//sent views to EJS
 
-//this is how a route is built in Express
+//route for index page
 app.get('/', (req,res)=>{
   var title = "Our Home Page";
-  res.render('index',{title:title});
+  res.render('pages/index',{title:title});
 });
 
+//route for users (list) page
+app.get('/users', function(req, res){
+  var title = 'Users page';
+  res.render('users/index',{
+    title: title,
+    users: data
+  });
+});
+
+//add user/view route - we are cheating by using the array index + 1
+app.get('/users/view/:id', function(req, res) {
+  var title = 'User Page';
+  var id = req.params.id;
+  res.render('users/view', {
+      title: title,
+      user: data[--id]
+  });
+ });
+
+ 
+ 
 app.listen(PORT,()=>{
   console.log(`App is running on port ${PORT}`);
+  console.log(data);
 });
 
